e752a2ccc9af04b3f0db58d2d91574ebf1e45bc4
Iamneo <techadmin@iamneo.ai>
2023-08-28 06:29:11 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,12 @@
+import logo from './logo.svg';
 import './App.css';
-
+import {useState} from react;
 function App() {
+
   return (
     <div>
       <h1 style={{"text-align": "center"}}>Quiz App</h1>
-      <button class="buttons">Start Quiz</button>
+      <button class="buttons" style={{"font-weight": "bold"}}>Start Quiz</button>
     </div>
   );
 }
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   // <App />,
-//   <h1>Hello</h1>,
-//   document.getElementById('root')
-// );
-document.getElementById("root").innerHTML="abc";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
