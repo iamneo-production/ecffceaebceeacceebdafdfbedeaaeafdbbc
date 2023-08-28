065f5829d7473578c6f45291f34e4b6d2ef3139e
Iamneo <techadmin@iamneo.ai>
2023-08-28 06:34:11 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from react;
+import {useState} from "react";
 function App() {
-
+  var [choice,changeChoice]=useState(0);
   return (
     <div>
       <h1 style={{"text-align": "center"}}>Quiz App</h1>
-      <button class="buttons" style={{"font-weight": "bold"}}>Start Quiz</button>
+      <h1>{{choice}}</h1>
+      <button class="buttons" style={{"font-weight": "bold"}} onClick={{()=>{changeChoice(1);}}}>Start Quiz</button>
     </div>
   );
 }
