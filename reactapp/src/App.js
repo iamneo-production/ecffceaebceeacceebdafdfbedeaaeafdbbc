418212c57c5cb15e0b7a4c574ac7c2ea8e2b838a
Iamneo <techadmin@iamneo.ai>
2023-09-02 15:53:46 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,6 +5,7 @@ function App(){
     var [states,setStates]=useState({
         curpage: "strt",
         tot_cor_ans: 0,
+        attempted: 0,
         questions: [
             {
                 Question: "Who is the father of nation ?",
@@ -25,17 +26,11 @@ function App(){
                 isDisabled: ""
             },
             {
-                Question: "What color is the sky ?",
+                Question: "What color is the fire ?",
                 Options: ["Blue","Red","Yellow","Green"],
-                correctAns: "4",
+                correctAns: "3",
                 isDisabled: ""
             },
-            {
-                Question: "Who is the father of nation?",
-                Options: ["Blue","Red","Yellow","Green"],
-                correctAns: "4",
-                isDisabled: ""
-            }
         ]
     });
     const setAns=(qno,op_no)=>{
@@ -49,7 +44,7 @@ function App(){
         if(states.questions[qno].correctAns===op_no){
         prv+=1;
         }
-        setStates({...states,tot_cor_ans: prv,prv_ques});
+        setStates({...states,tot_cor_ans: prv,prv_ques,attempted: });
         console.log(states.questions,op_no,qno);
     }
     return (
@@ -60,9 +55,9 @@ function App(){
             <button style={{backgroundColor: "#FFBA93",border: "1px solid #FFBA93",padding: "5px",width: "35%",height: "45px"}} onClick={()=>setStates({...states,curpage: "quiz"})}>Start Quiz</button> : ""
             }
             <Banner question={states.questions[0].Question} tag="1" options={states.questions[0].Options} setAns={setAns} isDisabled={states.questions[0].isDisabled}/>
-            <Banner question={states.questions[0].Question} tag="2" options={states.questions[1].Options} setAns={setAns} isDisabled={states.questions[1].isDisabled}/>
-            <Banner question={states.questions[0].Question} tag="3" options={states.questions[2].Options} setAns={setAns} isDisabled={states.questions[2].isDisabled}/>
-            <Banner question={states.questions[0].Question} tag="4" options={states.questions[3].Options} setAns={setAns} isDisabled={states.questions[3].isDisabled}/>
+            <Banner question={states.questions[1].Question} tag="2" options={states.questions[1].Options} setAns={setAns} isDisabled={states.questions[1].isDisabled}/>
+            <Banner question={states.questions[2].Question} tag="3" options={states.questions[2].Options} setAns={setAns} isDisabled={states.questions[2].isDisabled}/>
+            <Banner question={states.questions[3].Question} tag="4" options={states.questions[3].Options} setAns={setAns} isDisabled={states.questions[3].isDisabled}/>
             {/* <Button tag="1" choice="1" name="hlo" content="Start Quiz" isdisabled="" onclick={setAns}/> */}
         </div>
     )
