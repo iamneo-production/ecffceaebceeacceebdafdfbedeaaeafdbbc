03d8b20d8da57d35d81b6089b136eb689660033d
Iamneo <techadmin@iamneo.ai>
2023-09-02 12:13:35 +0000

    Backup Commit

--- reactapp/src/samp.test.js
+++ reactapp/src/samp.test.js
@@ -1,22 +1,10 @@
 import React from "react";
-import App from "../App";
+import App from "./App";
 import { shallow, mount } from "enzyme";
 
 
 var wrapper;
 describe('Testing App Component', () => {
-    // test('testcase1', () => {
-    //     wrapper = mount(<App />);
-    //     expect(wrapper.instance()).toBeDefined();
-    //     wrapper.unmount();
-    // });
-
-    // test('testcase2', () => {
-    //     wrapper = mount(<App />);
-    //     expect(wrapper.find('h1').text()).toBe('Quizz App');
-    //     expect(wrapper.find('button').instance()).toBeDefined();
-    //     wrapper.unmount();
-    // });
 
     test('testcase3', () => {
         wrapper = mount(<App />);
@@ -27,25 +15,5 @@ describe('Testing App Component', () => {
         wrapper.unmount();
     });
 
-    test('testcase4', () => {
-        const wrapper = mount(<App />);
-        wrapper.find('button').simulate('click')
-        wrapper.find('button').forEach(node => {
-            expect(node.simulate('click'));
-        });
-        
-        expect(wrapper.find('Button').last().text()).toBe('Show Results');
-        wrapper.unmount();
-    });
-
-    test('testcase5', () => {
-        const wrapper = mount(<App />);
-        wrapper.find('button').simulate('click')
-        wrapper.find('button').forEach(node => {
-            expect(node.simulate('click'));
-        });
-        wrapper.find('button').last().simulate('click');
-        expect(wrapper.find('button').text()).toBe('Start Quiz');
-
-    })
+    
 })
