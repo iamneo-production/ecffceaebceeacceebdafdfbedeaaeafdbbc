d15459cb9199678a60e75fd8981f3110a9c4bfb8
Iamneo <techadmin@iamneo.ai>
2023-09-02 15:23:45 +0000

    Backup Commit

--- reactapp/src/components/UI/Banner/Banner.js
+++ reactapp/src/components/UI/Banner/Banner.js
@@ -2,8 +2,11 @@ import Button from "../Button/Button";
 function Banner(props){
     return(
         <div className="outerdiv">
-            <p>props.question</p>
-            {/* <Button tag={props.tag} choice="1" className="buttons" content={props.options[0]} isdisabled={props.disabled} onclick={props.setAns}/> */}
+            <p>{props.question}</p>
+            <div style={{display: "flex"}}>
+            <Button tag={props.tag} choice="1" className="buttons" content={props.options[0]} isdisabled={props.disabled} onclick={props.setAns}/>
+            </div>
         </div>
     )
 }
+export default Banner;
