b65cb6edfa8db80ae9889293f8258bd9e13557c3
Iamneo <techadmin@iamneo.ai>
2023-09-02 15:13:43 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,16 +1,17 @@
 import {useState} from "react";
 import Button from "./components/UI/Button/Button";
+import Banner from "./components/UI/Banner/Banner";
 function App(){
     var [states,setStates]=useState({
         curpage: "strt",
         tot_cor_ans: 0,
         questions: [
             {
-                "Question": "Who is the father of nation?",
-                "1":"Nehru",
-                "2":"Gandhi",
-                "3":"Ambedkar",
-                "4":"Bose",
+                Question: "Who is the father of nation?",
+                1:"Nehru",
+                2:"Gandhi",
+                3:"Ambedkar",
+                4:"Bose",
                 correctAns: "4"
             }
         ]
@@ -29,7 +30,7 @@ function App(){
             {states.curpage==="strt" ? 
             <button style={{backgroundColor: "#FFBA93",border: "1px solid #FFBA93",padding: "5px",width: "50%"}} onClick={()=>setStates({...states,curpage: "quiz"})}>Start Quiz</button> : ""
             }
-            
+            <Banner question={states.}/>
             {/* <Button tag="1" choice="1" name="hlo" content="Start Quiz" isdisabled="" onclick={setAns}/> */}
         </div>
     )
--- reactapp/src/components/UI/Banner/Banner.js
+++ reactapp/src/components/UI/Banner/Banner.js
@@ -1,1 +1,9 @@
 import Button from "../Button/Button";
+function Banner(props){
+    return(
+        <div className="outerdiv">
+            <p>props.question</p>
+            <Button tag={props.tag} choice="1" name="buttons" content={props.options[0]} isdisabled={props.disabled} onclick={props.setAns}/>
+        </div>
+    )
+}
