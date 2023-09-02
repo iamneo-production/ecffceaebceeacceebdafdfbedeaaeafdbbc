85a5198520f5ea95289cd0e88a4235d6dc5ff3aa
Iamneo <techadmin@iamneo.ai>
2023-09-02 15:28:45 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -10,6 +10,26 @@ function App(){
                 Question: "Who is the father of nation?",
                 Options: ["Nehru","Gandhi","Ambedkar","Bose"],
                 correctAns: "4"
+            },
+            {
+                Question: "Who is the father of nation?",
+                Options: ["Nehru","Gandhi","Ambedkar","Bose"],
+                correctAns: "4"
+            },
+            {
+                Question: "Who is the father of nation?",
+                Options: ["Nehru","Gandhi","Ambedkar","Bose"],
+                correctAns: "4"
+            },
+            {
+                Question: "Who is the father of nation?",
+                Options: ["Nehru","Gandhi","Ambedkar","Bose"],
+                correctAns: "4"
+            },
+            {
+                Question: "Who is the father of nation?",
+                Options: ["Nehru","Gandhi","Ambedkar","Bose"],
+                correctAns: "4"
             }
         ]
     });
@@ -22,12 +42,15 @@ function App(){
         console.log("Here",qno,states.questions[qno].correctAns,op_no);
     }
     return (
-        <div style={{textAlign: "center"}}>
-            <h1>Quizz App</h1>
+        <div >
+            <h1 style={{textAlign: "center"}}>Quizz App</h1>
             {states.curpage==="strt" ? 
-            <button style={{backgroundColor: "#FFBA93",border: "1px solid #FFBA93",padding: "5px",width: "50%"}} onClick={()=>setStates({...states,curpage: "quiz"})}>Start Quiz</button> : ""
+            <button style={{backgroundColor: "#FFBA93",border: "1px solid #FFBA93",padding: "5px",width: "35%",height: "45px"}} onClick={()=>setStates({...states,curpage: "quiz"})}>Start Quiz</button> : ""
             }
             <Banner question={states.questions[0].Question} tag="1" options={states.questions[0].Options}/>
+            <Banner question={states.questions[1].Question} tag="2" options={states.questions[1].Options}/>
+            <Banner question={states.questions[2].Question} tag="3" options={states.questions[2].Options}/>
+            <Banner question={states.questions[3].Question} tag="4" options={states.questions[3].Options}/>
             {/* <Button tag="1" choice="1" name="hlo" content="Start Quiz" isdisabled="" onclick={setAns}/> */}
         </div>
     )
--- reactapp/src/components/UI/Banner/Banner.js
+++ reactapp/src/components/UI/Banner/Banner.js
@@ -5,6 +5,9 @@ function Banner(props){
             <p>{props.question}</p>
             <div style={{display: "flex"}}>
             <Button tag={props.tag} choice="1" className="buttons" content={props.options[0]} isdisabled={props.disabled} onclick={props.setAns}/>
+            <Button tag={props.tag} choice="2" className="buttons" content={props.options[1]} isdisabled={props.disabled} onclick={props.setAns}/>
+            <Button tag={props.tag} choice="3" className="buttons" content={props.options[2]} isdisabled={props.disabled} onclick={props.setAns}/>
+            <Button tag={props.tag} choice="4" className="buttons" content={props.options[3]} isdisabled={props.disabled} onclick={props.setAns}/>
             </div>
         </div>
     )
