cafa1dfbc0a90ddff621dc583f5c588d3a315aed
Iamneo <techadmin@iamneo.ai>
2023-09-02 14:38:43 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -7,20 +7,24 @@ function App(){
         questions: [
             {
                 "Question": "Who is the father of nation?",
-                
+                "Option1":"Nehru",
+                "Option2":"Gandhi",
+                "Option3":"Ambedkar",
+                "Option4":"Bose",
+                "CorrectAns": "4"
             }
         ]
     });
-    function setAns(qno){
-
-        if(states.qno[qno].correctAns===curans){
-        setStates({...states,correctAnswer: 1});
+    function setAns(qno,opno){
+        var prv=states.correctAnswer;
+        if(states.questions[qno].correctAnswer===opno){
+        setStates({...states,correctAnswer: prv+1});
         }
     }
     return (
         <div>
             <h1>Quizz App</h1>
-            <Button name="hlo" content="123" isdisabled="true" onClick={this.setAns(1)}/>
+            <Button name="hlo" content="123" isdisabled="true" onClick={()=>{this.setAns(1)}}/>
         </div>
     )
 }
