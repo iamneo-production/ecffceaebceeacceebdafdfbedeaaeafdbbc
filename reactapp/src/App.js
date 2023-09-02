3607a25dc3ec975e9115ded8803ea5165881b8ad
Iamneo <techadmin@iamneo.ai>
2023-09-02 14:18:42 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,21 +1,20 @@
 import {useState} from "react";
 import Button from "./components/UI/Button/Button";
 function App(){
-    var states,setState=useState({
+    var Allstates,setStates=useState({
         curpage: "strt",
         correctAnswer: 0,
 
     });
     function setAns(qno,curans){
         if(states.qno[qno].correctAns==curans){
-        setState({...states,correctAnswer: 1});
+        setStates({...states,correctAnswer: 1});
         }
     }
     return (
         <div>
             <h1>Quizz App</h1>
-            <div>Correct Ans {states.correctAns}</div>
-            <Button name="button" content="Start Quiz"/>
+            <div>Correct Ans {Allstates.}</div>
         </div>
     )
 }
