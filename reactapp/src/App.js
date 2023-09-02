b2b924e3bd29b22d7ebad06d8935ff1c778dfa79
Iamneo <techadmin@iamneo.ai>
2023-09-02 14:03:41 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,18 @@
+import {useState} from "react";
+function App(){
+    states,setState=useState({
+        curpage: "strt",
+        currectAnswer: 0,
+
+    });
+    function setAns(qno){
+        if(states.qno[0].correctAns==)
+        setState({...states,currectAnswer: currectAnswer+1})
+    }
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default App;
