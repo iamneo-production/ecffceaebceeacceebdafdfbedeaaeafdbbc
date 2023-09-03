514e7ec5a9c7933c7d012238dd40a5404d9c4771
Iamneo <techadmin@iamneo.ai>
2023-09-03 07:49:47 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -4,10 +4,10 @@ function Card(props){
         <div className="outerdiv">
             <h4>{props.question}</h4>
             <div style={{display: "flex"}}>
-            <Button tag={props.key} answer={props.answer} choice="1" className="buttons" content={props.options.option1} isdisabled={props.isDisabled} onclick={props.correctAnswerMarkUpdate}/>
-            <Button tag={props.key} answer={props.answer} choice="2" className="buttons" content={props.options.option2} isdisabled={props.isDisabled} onclick={props.correctAnswerMarkUpdate}/>
-            <Button tag={props.key} answer={props.answer} choice="3" className="buttons" content={props.options.option3} isdisabled={props.isDisabled} onclick={props.correctAnswerMarkUpdate}/>
-            <Button tag={props.key} answer={props.answer} choice="4" className="buttons" content={props.options.option4} isdisabled={props.isDisabled} onclick={props.correctAnswerMarkUpdate}/>
+            <Button answer={props.answer} choice="1" className="buttons" content={props.options.option1} isdisabled={props.isDisabled} onclick={props.correctAnswerMarkUpdate}/>
+            <Button answer={props.answer} choice="2" className="buttons" content={props.options.option2} isdisabled={props.isDisabled} onclick={props.correctAnswerMarkUpdate}/>
+            <Button answer={props.answer} choice="3" className="buttons" content={props.options.option3} isdisabled={props.isDisabled} onclick={props.correctAnswerMarkUpdate}/>
+            <Button answer={props.answer} choice="4" className="buttons" content={props.options.option4} isdisabled={props.isDisabled} onclick={props.correctAnswerMarkUpdate}/>
             </div>
         </div>
     )
