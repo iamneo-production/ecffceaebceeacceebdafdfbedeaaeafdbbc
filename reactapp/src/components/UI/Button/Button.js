18b034a779b4732f1613205438d0b8fb732b3285
Iamneo <techadmin@iamneo.ai>
2023-09-03 07:59:47 +0000

    Backup Commit

--- reactapp/src/components/UI/Button/Button.js
+++ reactapp/src/components/UI/Button/Button.js
@@ -1,6 +1,9 @@
 function Button(props){
     return(
-        <button disabled={props.isdisabled} onClick={()=>{props.onclick(props.tag,props.content,props.answer)}}>{props.content}</button>
+        <div>
+        <button id={props.id} onClick={()=>{props.onclick(props.tag,props.content,props.answer)}} disabled={props.isdisabled} >{props.content}</button>
+        {/* <button disabled={props.isdisabled} onClick={()=>{props.onclick(props.tag,props.content,props.answer)}}>{props.content}</button> */}
+        </div>
     )
 }
 export default Button;
